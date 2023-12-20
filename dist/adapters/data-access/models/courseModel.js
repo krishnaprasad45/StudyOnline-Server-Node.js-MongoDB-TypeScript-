cd33554c472459fdc4c18475171793f6c37203fe
krishna prasad <krishnaprasadc45@gmail.com>
2023-12-20 12:57:52 +0530

    recent updates

---
+++ dist/adapters/data-access/models/courseModel.js
@@ -0,0 +1,45 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const courseSchema = new mongoose_1.default.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    fee: {
+        type: Number,
+        required: true,
+    },
+    createdby: {
+        type: String,
+        required: true,
+    },
+    createdat: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    banner: {
+        type: String,
+        required: true,
+    },
+    introvideo: {
+        type: String,
+        required: true,
+    },
+});
+const Course = mongoose_1.default.model('course', courseSchema);
+exports.default = Course;
